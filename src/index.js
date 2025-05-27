6938056b62bc0304b81c1a0384c7a0a134c6b66b
WaithakaGuru <waithakaoffices@gmail.com>
2025-05-27 21:15:26 +0300

    fix the fetch name parameter bug resulted to empty response prior

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const apiLink = "https://api.nationalize.io/?name=";
 const getCountryNames = new Intl.DisplayNames(["en"], { type: "region" });
 
 const getNationality = async (nameInfo) => {
-  const request = await fetch(`${apiLink}+${nameInfo}`);
+  const request = await fetch(`${apiLink}${nameInfo}`);
   const results = await request.json();
   return results;
 };
@@ -20,39 +20,37 @@ async function display(name) {
         return;
       }
 
+      submitBtn.innerHTML = "Searching";
+      submitBtn.setAttribute("disabled", true);
       const countries = response.country;
-      resultsHolder.innerHTML = `<b class="name-holder">${name}</b> is likely from: <ul>`;
+      resultsHolder.innerHTML = `<b class="name-holder"><u>'${name}'</u> is likely from:</b> <ul>`;
 
       for (let i = 0; i < Math.min(5, countries.length); i++) {
         const { country_id, probability } = countries[i];
         resultsHolder.innerHTML += `<li class="item">${getCountryNames.of(country_id)} — ${Math.round(probability * 100)}% sure</li>`;
       }
       resultsHolder.innerHTML += `</ul>`;
+      submitBtn.removeAttribute("disabled");
+      submitBtn.innerHTML = `<i class="fa-solid fa-magnifying-glass"></i>`;
     } catch (error) {
       console.error("Error in display():", error);
       resultsHolder.innerHTML = `An error occurred while fetching nationality data. Please try again later.`;
     }
 }
 
-
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const nameInfo = nameInput.value;
   try {
-    if (nameInfo === '') {
+    if (nameInfo.trim().toLowerCase() === '') {
       resultsHolder.textContent = "Please enter a name!!";
       return;
     } else {
-      submitBtn.innerHTML = "Searching";
-      submitBtn.setAttribute("disabled", true);
+      resultsHolder.textContent = '';
       display(nameInfo);    
     }
   } catch (e) {
-        // console.log(e.message);
-        resultsHolder.textContent = `${e}` +
-        "ERROR: Something went Wrong. Try Again Later!!";
-  }finally{
-        submitBtn.removeAttribute("disabled");
-        submitBtn.innerHTML = `<i class="fa-solid fa-magnifying-glass"></i>`;
+    resultsHolder.textContent =
+    "ERROR: Something went Wrong. Try Again Later!!";
   }
 });
